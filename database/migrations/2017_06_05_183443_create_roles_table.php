622ee99b287ca1e6bf66cea0023b243055cae891
William <jamiepatrickson@hotmail.co.uk>
2017-06-05 19:59:36 +0100

    Creating user roles table in the migrations

---
+++ database/migrations/2017_06_05_183443_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Creating a Roles table to store the different user roles that equate to different application permissions
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
