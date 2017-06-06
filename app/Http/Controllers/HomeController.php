4feb822fc6b7625442ce64e2b87379ed3e26665b
William <jamiepatrickson@hotmail.co.uk>
2017-06-06 20:53:39 +0100

    Passing user information including role from the controller to the view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Role;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = $this->getUserInfo();
+        return view('home', ["data" => $data]);
+    }
+
+    /**
+     * Function to retrieve return user information
+     *
+     */
+     
+    public function getUserInfo(){
+
+        return array(
+          "name" => Auth::user()->name,
+          "email" => Auth::user()->email,
+          "role" => $this->getRole(Auth::user()->role_id)
+        );
+
+    }
+
+    /**
+     * Function to retrieve role title from id
+     *
+     */
+
+    public function getRole($id){
+        $result = Role::where("id", $id)->first();
+        return $result->title;
     }
 }
